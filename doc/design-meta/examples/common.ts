4b24eb05d995e8979815c5cd99ac180ee2c6e1f7
Olivier Huin <olih@users.noreply.github.com>
2026-05-11 09:28:32 +0100

    Refactor validation

--- doc/design-meta/examples/common.ts
+++ doc/design-meta/examples/common.ts
@@ -1,2 +1,9 @@
 export type UUID = string;
 export type Version = string;
+export type KeyStructKind = string //note:text:comment
+
+
+export type Command = {
+    name: string;
+    args: string[];
+}
--- doc/design-meta/examples/validation.ts
+++ doc/design-meta/examples/validation.ts
@@ -1,19 +1,5 @@
-type KeyStructKind = string //note:text:comment
-type validationPhase = 'BeforeValidation' | 'AfterValidation'
-
-type Validation = {
-    args: string[];
-    stop: boolean;//if fails
-    tags: string[];//warning
-    phase: validationPhase;
-}
-
-type Formatting = {
-    args: string[];//trim
-    tags: string[];//warning
-}
+import { Command, KeyStructKind } from "./common";
 
 export type ValidationSchema = {
-  validationByKeyStruct: Record<KeyStructKind, Validation[]>;
-  formattingByKeyStruct: Record<KeyStructKind, Formatting[]>;
+  commandByKeyStruct: Record<KeyStructKind, Command[]>;
 };
