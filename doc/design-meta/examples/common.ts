4fd4f64aea57615b904a2505a10e0c3146bbb1d0
Olivier Huin <olih@users.noreply.github.com>
2026-05-11 20:38:15 +0100

    Multiple commands support

--- doc/design-meta/examples/common.ts
+++ doc/design-meta/examples/common.ts
@@ -1,9 +1,9 @@
 export type UUID = string;
 export type Version = string;
 export type KeyStructKind = string //note:text:comment
+export type CommandKind = string;//validation,monitoring
 
 
 export type Command = {
-    name: string;
-    args: string[];
+    args: Record<CommandKind, string[]>;
 }
