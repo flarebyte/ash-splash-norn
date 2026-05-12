45ecac818d05c4e4c32c2801f1f0df482add6b59
Olivier Huin <olih@users.noreply.github.com>
2026-05-12 07:08:58 +0100

    Uses constraints

--- doc/design-meta/examples/common.ts
+++ doc/design-meta/examples/common.ts
@@ -3,7 +3,11 @@ export type Version = string;
 export type KeyStructKind = string //note:text:comment
 export type CommandKind = string;//validation,monitoring
 
+export type Constraints = {
+    schema: string[];
+    schemas: string[][];
+}
 
 export type Command = {
-    args: Record<CommandKind, string[]>;
+    args: Record<CommandKind, Constraints>;
 }
