f1496d76c73ae96f20015808748e7c084f2c36d9
Olivier Huin <olih@users.noreply.github.com>
2026-05-12 22:16:41 +0100

    uses CommandSpec

--- doc/design-meta/examples/model/common.ts
+++ doc/design-meta/examples/model/common.ts
@@ -1,15 +1,21 @@
 export type UUID = string;
 export type Version = string;
-export type KeyStructKind = string //note:text:comment
-export type CommandKind = string;//validation,monitoring
+export type KeyStructKind = string; //note:text:comment
+export type CommandKind = string; //validation,monitoring
 
-export type ConstraintSpec = {
-    kind: 'string' | 'number' | 'boolean' | 'tuple';
-    name: string;
-    schema: string[];
-    schemas: string[][];
-}
+export type FlagSpec = {
+  kind: "string" | "number" | "boolean" | "tuple";
+  name: string;
+  schema: string[];
+  schemas: string[][];
+};
+
+export type CommandSpec = {
+  commandPath: string[];
+  adminOnly: boolean;
+  flags: FlagSpec[];
+};
 
 export type Command = {
-    args: Record<CommandKind, ConstraintSpec>;
-}
+  args: Record<CommandKind, CommandSpecs>;
+};
